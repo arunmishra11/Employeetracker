04c6317287ddf335d2cfcf652245a957d1585948
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-03-11 21:26:19 -0500

    Adding my portfolio assignment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Employeetracker
+
+#Description#
+
+This project allows an individual to add the details of employees. Details includes their first name, last name and their salary. The code also calculates the average salary depending upon the number of the employees. At last, it randomly selects an employee as a winner.
+
+
+Methodologies used:
+
+HTML, CSS and js
+
+github link: https://github.com/arunmishra11/Employeetracker
+
---
+++ script.js
@@ -0,0 +1,134 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+const collectEmployees = function() {
+
+  const employees = [];
+  const employee = createEmployee();
+  employees.push(employee);
+ 
+  
+    while (confirm("Do you want to add another Employee?"))   {
+    const employee = createEmployee();
+    employees.push(employee);
+    
+   }
+    return employees;
+    
+};
+
+
+function createEmployee() {
+  const employee = {
+    firstName: "",
+    lastName: "",
+    salary: 0
+  };
+
+
+ 
+  employee.firstName = window.prompt("Enter first name")
+
+  employee.lastName = window.prompt("Enter last name")
+
+  employee.salary = window.prompt("Employee's salary")
+
+  if (isNaN(employee.salary) || employee.salary <0) {
+    employee.salary = 0;
+  }
+  else {
+  employee.salary = parseInt(employee.salary);
+  }
+  return employee;
+}
+
+
+
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+
+  let sum = 0;
+  for(let i = 0; i < employeesArray.length; i++) {
+    
+    sum += employeesArray[i].salary;
+  }
+  const average = sum / employeesArray.length;
+  return console.log(`Average employee salary is: ${average}`); 
+
+}
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  let r = Math.floor(Math.random() *employeesArray.length);
+  let randomEmployee = employeesArray[r].firstName + " " + employeesArray[r].lastName;
+  return console.log(`congratulations, ${randomEmployee}, you have randomly won!`);
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
+
